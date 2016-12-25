66c316a92d84cb170b3ed2aecb1e1cba439f6359
arnold Agus <arnold.agus@gmail.com>
2016-12-26 00:16:03 +1100

    Working json reading

--- teleporter.go
+++ teleporter.go
@@ -7,11 +7,18 @@ import (
 	// "bufio"
 	// "io"
 	"io/ioutil"
-	// "endcoding/json"
+	"encoding/json"
 )
 
+type configStruct struct {
+	Alias map[string]string `json:"alias"`
+}
+
+// type configStruct struct {
+// 	alias map[string]interface{}
+// }
+
 func handleErr(err error) {
-	fmt.Println("here is our error", err)
 	if err != nil {
 		fmt.Println("Shit our error is bad", err)
 		panic(err)
@@ -30,8 +37,23 @@ func main() {
 
 	fmt.Println(helpMessage)
 
-	_, err := ioutil.ReadFile("./config.json")
+	var config configStruct
+
+	
+	configBytes, err := ioutil.ReadFile("./config.json")
 	handleErr(err)
+
+	err2 := json.Unmarshal(configBytes, &config)
+	handleErr(err2)
+
+	fmt.Println("Here is our config")
+	fmt.Println(config)
+
+	alias := config.Alias["working"]
+	fmt.Println("here is our alias")
+	fmt.Println(alias)
+	// fmt.Println(config["alias"])
+	// fmt.Println(config["alias"]["working"])
 
 
 
