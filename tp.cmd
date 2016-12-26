c56d99669b8d6ca4430ccf2829a31fab3efba19a
arnold Agus <arnold.agus@gmail.com>
2016-12-26 22:36:00 +1100

    Adjustment to batch script

--- tp.cmd
+++ tp.cmd
@@ -1,11 +1,10 @@
 
 @echo off
 setlocal enabledelayedexpansion
-(tp %*) > tp_tmpfile.txt
+(teleporter %*) > tp_tmpfile.txt
 if %errorlevel% equ 2 (
     for /f "delims=" %%A in ('cat tp_tmpfile.txt') do set output=%%A
     del tp_tmpfile.txt
-    echo !output!
     cd /d !output!
     pushd .
     endlocal
